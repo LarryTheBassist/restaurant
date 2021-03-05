1189d0074dfd9021f6ee370b242ddebefc92652c
Lawrence <Lewman2726@gmail.com>
2021-03-05 00:47:54 -0500

    added content to page, set up javascript in index.js, added background image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log("Hello from the console!")
+const content = document.querySelector("#content");
+
+//create header
+const headline = document.createElement("h1");
+headline.textContent = "Larry's Restaurant";
+content.appendChild(headline);
+
+//set up image
+const bbqImage = document.createElement("img");
+bbqImage.src = "https://jesspryles.com/wp-content/uploads/2019/08/al-pastor-ribs-49-1440x900.jpg"
+content.appendChild(bbqImage);
+
+//set up text
+const textPara = document.createElement("p");
+textPara.textContent = "This restaurant is honestly so fucking good. I mean, I honestly don't know what more to tell you. Established in 2015, we are desperately trying to survive the 'rona."
+content.appendChild(textPara);
